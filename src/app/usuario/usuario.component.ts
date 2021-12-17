805617306236f474feaa4551b5ce31beac779862
EPNM <developer@fws.com>
2021-12-17 10:55:44 -0600

    pantalla usqueda

--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
 
 
 @Component({
@@ -7,6 +8,8 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./usuario.component.scss"],
   })
 export class UsuarioComponent implements OnInit {
+
+
 
     constructor( ) { }
 
